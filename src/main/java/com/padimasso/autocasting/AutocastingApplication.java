b32b53b1905e36ff8daf930fd79d0f02ae5c0ac0
Tomas Padilla <padillatomasagustin@gmail.com>
2025-06-19 11:20:21 +0200

    develop - base project + env yaml  y run config

---
+++ src/main/java/com/padimasso/autocasting/AutocastingApplication.java
@@ -0,0 +1,13 @@
+package com.padimasso.autocasting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutocastingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutocastingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/padimasso/autocasting/AutocastingApplicationTests.java
@@ -0,0 +1,14 @@
+package com.padimasso.autocasting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutocastingApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// Empty
+	}
+
+}
