c79f63a363e8199378216ea621bdca47f8573be7
Tomas Padilla <padillatomasagustin@gmail.com>
2025-07-13 16:17:50 +0200

    develop - Retoques al Script

--- src/main/resources/scripts/Insert Values.sql
+++ src/main/resources/scripts/Insert Values.sql
@@ -1,5 +1,5 @@
 ------------------------------------------------------------------------
----- Insert Roles 
+---- Insert Roles
 ------------------------------------------------------------------------
 INSERT INTO roles (
   id,
@@ -13,13 +13,13 @@ INSERT INTO roles (
   modified_by
 )
 
-VALUES 
-(gen_random_uuid(), 'ACTOR', 'role.actor', 'Usuario que representa un actor', true, NOW(), 'system', NOW(), 'SYSTEM'),
-(gen_random_uuid(), 'CASTINERA', 'role.castinera',  'Usuario que busca y gestiona actores', true, NOW(), 'system', NOW(), 'SYSTEM');
+VALUES
+(gen_random_uuid(), 'ACTOR', 'role.actor', 'Usuario que representa un actor', true, NOW(), 'SYSTEM', NOW(), 'SYSTEM'),
+(gen_random_uuid(), 'CASTINERA', 'role.castinera',  'Usuario que busca y gestiona actores', true, NOW(), 'SYSTEM', NOW(), 'SYSTEM');
 
 
 ------------------------------------------------------------------------
----- Insert Planes 
+---- Insert Planes
 ------------------------------------------------------------------------
 INSERT INTO plans (
   id,
@@ -33,7 +33,7 @@ INSERT INTO plans (
   modified_at,
   modified_by
 )
-VALUES 
+VALUES
   (gen_random_uuid(), 'FREE', 'plan.free', 'Plan gratuito para todos los usuarios', false, true, NOW(), 'SYSTEM', NOW(), 'SYSTEM'),
   (gen_random_uuid(), 'PREMIUM_ACTOR_1', 'plan.premium_actor_1', 'Permite slug personalizado', true, true, NOW(), 'SYSTEM', NOW(), 'SYSTEM'),
   (gen_random_uuid(), 'PREMIUM_CASTINERA', 'plan.premium_castinera', 'Acceso a búsquedas de actores', true, true, NOW(), 'SYSTEM', NOW(), 'SYSTEM');
