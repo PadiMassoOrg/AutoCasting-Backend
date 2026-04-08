7d179a799fb3ec9d9fd1650d3af9c327b0605c67
Tomas Padilla <padillatomasagustin@gmail.com>
2026-04-08 14:05:00 +0200

    [ACIT-31] - Skills OK

--- src/main/java/com/padimasso/autocasting/application/talent/dto/request/SkillsPatchRequest.java
+++ src/main/java/com/padimasso/autocasting/application/talent/dto/request/SkillsPatchRequest.java
@@ -1,9 +1,11 @@
 package com.padimasso.autocasting.application.talent.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.util.Set;
 import java.util.UUID;
 
 public record SkillsPatchRequest(
-    Set<UUID> skillIds
+    Set<@NotNull(message = "talent.skills_required") UUID> skillIds
 ) {
 }
