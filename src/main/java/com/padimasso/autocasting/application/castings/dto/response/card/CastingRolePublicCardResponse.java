2a99170cbaade3fcfdcef17cc35cf714c6d0d9ea
Tomas Padilla <padillatomasagustin@gmail.com>
2026-05-17 15:10:43 +0200

    [ACIT-51] Casting Catalog, nuevo funcionamiento. Falta: 1) Limpiar codigo inutilizado 2) Refinar Data enviada 3) Mejorar el SEEDER.

--- src/main/java/com/padimasso/autocasting/application/castings/dto/response/card/CastingRolePublicCardResponse.java
+++ src/main/java/com/padimasso/autocasting/application/castings/dto/response/card/CastingRolePublicCardResponse.java
@@ -3,7 +3,6 @@ package com.padimasso.autocasting.application.castings.dto.response.card;
 import com.padimasso.autocasting.application.sitemetadata.dto.response.SiteMetadataObject;
 
 import java.time.LocalDate;
-import java.util.List;
 import java.util.UUID;
 
 public record CastingRolePublicCardResponse(
@@ -14,15 +13,11 @@ public record CastingRolePublicCardResponse(
     String castingTitle,
     // Employer Basic Info
     String employerImageUrl,
-    String employerCompanyName,
     // Casting Basic Info
     SiteMetadataObject projectType,
-    SiteMetadataObject castingModality,
-    String location,
     LocalDate shootingStartDate,
     LocalDate shootingEndDate,
     // Casting Role
-    List<SiteMetadataObject> professions,
     SiteMetadataObject roleType,
     SiteMetadataObject gender,
     Short ageMin,
